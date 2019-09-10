bc3ccc08a5297b7ec8512a0b8d4dc3b55f0de95e
Jonny Ramen <jpper@umich.edu>
2019-09-10 14:57:33 +0900

    Implemented realtime display of message object on send

--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -143,65 +143,23 @@
         </div>
         <div class="mesgs">
           <div class="msg_history">
-            <div class="incoming_msg">
-              <div class="incoming_msg_img">
-                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-              </div>
+            <div v-for="message in messages" class="incoming_msg">
               <div class="received_msg">
                 <div class="received_withd_msg">
-                  <p>
-                    Test which is a new approach to have all
-                    solutions
-                  </p>
-                  <span class="time_date">11:01 AM | June 9</span>
-                </div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>
-                  Test which is a new approach to have all
-                  solutions
-                </p>
-                <span class="time_date">11:01 AM | June 9</span>
-              </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img">
-                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-              </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>Test, which is a new approach to have</p>
-                  <span class="time_date">11:01 AM | Yesterday</span>
-                </div>
-              </div>
-            </div>
-            <div class="outgoing_msg">
-              <div class="sent_msg">
-                <p>Apollo University, Delhi, India Test</p>
-                <span class="time_date">11:01 AM | Today</span>
-              </div>
-            </div>
-            <div class="incoming_msg">
-              <div class="incoming_msg_img">
-                <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil" />
-              </div>
-              <div class="received_msg">
-                <div class="received_withd_msg">
-                  <p>
-                    We work directly with our designers and suppliers,
-                    and sell direct to you, which means quality, exclusive
-                    products, at a price anyone can afford.
-                  </p>
-                  <span class="time_date">11:01 AM | Today</span>
+                  <p>{{message.message}}</p>
                 </div>
               </div>
             </div>
           </div>
           <div class="type_msg">
             <div class="input_msg_write">
-              <input type="text" class="write_msg" placeholder="Type a message" />
+              <input
+                @keyup.enter="saveMessage"
+                v-model="message"
+                type="text"
+                class="write_msg"
+                placeholder="Type a message"
+              />
               <button class="msg_send_btn" type="button">
                 <i class="fa fa-paper-plane-o" aria-hidden="true"></i>
               </button>
@@ -209,11 +167,6 @@
           </div>
         </div>
       </div>
-
-      <p class="text-center top_spac">
-        Design by
-        <a target="_blank" href="#">Sunil Rajput</a>
-      </p>
     </div>
   </div>
 </template>
@@ -223,7 +176,37 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  components: {}
+  name: "PrivateChat",
+  data() {
+    return {
+      message: null,
+      messages: []
+    };
+  },
+  methods: {
+    saveMessage() {
+      // Save to Firestore
+      db.collection("chat").add({
+        message: this.message,
+        createdAt: new Date()
+      });
+      this.message = null;
+    },
+    fetchMessages() {
+      db.collection("chat")
+        .orderBy("createdAt")
+        .onSnapshot(querySnapshot => {
+          let allMessages = [];
+          querySnapshot.forEach(doc => {
+            allMessages.push(doc.data);
+          });
+          this.messages = allMessages;
+        });
+    }
+  },
+  created() {
+    this.fetchMessages();
+  }
 };
 </script>
 
