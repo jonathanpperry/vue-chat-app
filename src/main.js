88c92d021c11da64b382412832ee6b9f988632ad
Jonny Ramen <jpper@umich.edu>
2019-09-10 13:28:57 +0900

    First commit of vue chat app

---
+++ src/main.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import firebase from "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDs2zfDy1pG5pKmBZJJWTggP7yzBKsCYN4",
+  authDomain: "vue-chat-8f583.firebaseapp.com",
+  databaseURL: "https://vue-chat-8f583.firebaseio.com",
+  projectId: "vue-chat-8f583",
+  storageBucket: "",
+  messagingSenderId: "439310740690",
+  appId: "1:439310740690:web:a57751f788b7121f08510b"
+};
+firebase.initializeApp(firebaseConfig);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount("#app");
