769c4457970914c9ef481407f54da5798c03c856
Jonny Ramen <jpper@umich.edu>
2019-09-30 12:41:16 +0900

    Removed useless comment

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -21,7 +21,6 @@ export default {
           var token = result.credential.accessToken;
           // The signed-in user info.
           var user = result.user;
-          // ...
           this.$router.push("/chat");
         })
         .catch(function(error) {
